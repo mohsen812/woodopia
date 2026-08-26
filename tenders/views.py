550fa584bb0677a85274c05742ebb79d1a0b45c1
mohsen812 <mohsen.feelgood@gmail.com>
2026-08-26 12:52:21 +0330

    MVP-01: Add tender API foundation

--- tenders/views.py
+++ tenders/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Tender, Bid
+
+from .serializers import (
+    TenderSerializer,
+    BidSerializer,
+)
+
+
+class TenderListCreateView(
+    generics.ListCreateAPIView
+):
+
+    queryset = (
+        Tender.objects
+        .all()
+        .order_by("-created_at")
+    )
+
+    serializer_class = TenderSerializer
+
+
+
+class TenderDetailView(
+    generics.RetrieveAPIView
+):
+
+    queryset = Tender.objects.all()
+
+    serializer_class = TenderSerializer
+
+
+
+class BidCreateView(
+    generics.CreateAPIView
+):
+
+    queryset = Bid.objects.all()
+
+    serializer_class = BidSerializer
